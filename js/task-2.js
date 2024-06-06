5af654e385e66adba8aa74c8e06e129bd19f5b06
maciejbr <m.broniarski@gmail.com>
2024-06-06 19:17:58 +0200

    add stuff

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
