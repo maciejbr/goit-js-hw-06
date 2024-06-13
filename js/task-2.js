c30007eaca8a972ad910192db240029041f92409
maciejbr <m.broniarski@gmail.com>
2024-06-13 20:58:37 +0200

    final version

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,21 @@
 "use strict";
 
+class Storage {
+  #items = [];
+  constructor(array) {
+    this.#items = [...array];
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter((item) => item !== itemToRemove);
+  }
+}
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,24 @@
 "use strict";
 
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value += str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
 builder.padStart("^");
